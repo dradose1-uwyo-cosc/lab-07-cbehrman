37c1216a3ef703fb32adf545f586c31f59dd005f
cbehrman <cbehrman@uwyo.edu>
2024-10-30 18:41:07 +0000

    calculator finnished

--- cosc1010-lab07.py
+++ cosc1010-lab07.py
@@ -9,54 +9,80 @@
 # here
 
 
-# Prompt the user for an upper bound 
-# Write a while loop that gives the factorial of that upper bound
-# This will need to be a positive number
-# For this you will need to check to ensure that the user entered a number
-    # To do so you can use the methods `.isdigit()` or `.isnumeric()`
-    # If a user did not enter a number output a statement saying so
-# You will continue to prompt the user until a proper integer value is entered
+#This code below will just prompt the person using the code to use a positive number
+#There will be a lot of if, elif, or else statements just due to different stuff the user can input
+while True:
+    upper_bound = input("Enter a positive integer for the upper bound (or 'exit' to quit): ")
+    if upper_bound.lower() == 'exit':
+        break
+    if upper_bound.isdigit():
+        upper_bound = int(upper_bound)
+        if upper_bound < 0:
+            print("Please enter a positive integer.")
+        else:
+            factorial = 1
+            for i in range(1, upper_bound + 1):
+                factorial *= i
+            print(f"The result of the factorial based on the given bound {upper_bound} is {factorial}")
+            break
+    else:
+        print("That is not a valid number. Please try again.")
+#I really dont know if I did that first part right, but im pretty sure that the rest of it is correct
+#I had to use chatgpt for the code above because I do not know what a factorial is, so i dont know if the code is right
+print("*" * 75)
 
-factorial = 1
-
-print(f"The result of the factorial based on the given bound is {factorial}")
-
-print("*"*75)
-# Create a while loop that prompts a user for input of an integer values
-# Sum all inputs. When the user enters 'exit' (regardless of casing) end the loop
-# Upon ending the loop print the sum
-# Your program should accept both positive and negative input
-# Remember all inputs from stdin are strings, so you will need to convert the string to an int first
-# Before you convert the number you need to check to ensure that it is a numeric string
-    # To do so you can use the methods `.isdigit()` or `.isnumeric()`
-    # This will return true if every digit in your string is a numerical character
-    # However, that means a string such as `-1` would return false, even though your program should accept negative values
-    # This means you will need to have a check to see if `-` is first character of the string before you check if it is numerical
-    # If it is in the string you will need to remove the `-` character, and know that it will be a negative number, so a subtraction from the overall sum
-    # I recommend checking out: https://www.w3schools.com/python/ref_string_replace.asp to figure out how one may remove a character from a string
-# All this together means you will have an intensive while loop that includes multiple if statements, likely with some nesting 
-# The sum should start at 0 
-
-num_sum = 0 
+#This below is just a calculator but for adding
+num_sum = 0
+while True:
+    user_input = input("Enter numbers to sum (or type 'exit' to finish): ")
+    if user_input.lower() == 'exit':
+        break
+    if user_input:
+        if user_input[0] == '-':
+            if user_input[1:].isdigit():
+                num_sum -= int(user_input[1:])
+            else:
+                print("Invalid input. Please enter a valid number.")
+        elif user_input.isdigit():
+            num_sum += int(user_input)
+        else:
+            print("Invalid input. Please enter a valid number.")
+    else:
+        print("Input cannot be empty.")
 
 print(f"Your final sum is {num_sum}")
 
-print("*"*75)
-# Now you will be creating a two operand calculator
-# It will support the following operators: +,-,/,*,% 
-# So accepted input is of the form `operand operator operand` 
-# You can assume that the user is competent and will only input strings of that form 
-# You will again need to verify that the operands are numerical values
-# For this assume only positive integers will be entered, no need look for negative numbers 
-# You will need to check the string for which operator it contains
-# Once you do, you will need to remove the operands from the string
-# This can be done in multiple ways:
-    # You can go through the string in a loop and create a substring of the characters until an operator is reached
-        # Upon reaching the operator you will switch to another substring and add all characters following to the second new string 
-    # Alternatively you can use the `.split()` method to split the string around an operator: https://www.w3schools.com/python/ref_string_split.asp
-# Your program will need to work with whatever spacing is given  
-    # So, it should function the same for `5 + 6` as `5+6`
-# Print the result of the equation
-# Again, loop through prompting the user for input until `exit` in any casing is input 
+print("*" * 75)
+
+#This code below serves as a calulator with more than just summing
+while True:
+    calculation = input("Enter a calculation (or type 'exit' to finish): ")
+    if calculation.lower() == 'exit':
+        break
+    try:
+        # Identify the operator and split operands
+        for operator in ['+', '-', '*', '/', '%']:
+            if operator in calculation:
+                operands = calculation.replace(' ', '').split(operator)
+                if len(operands) == 2 and operands[0].isdigit() and operands[1].isdigit():
+                    operand1 = int(operands[0])
+                    operand2 = int(operands[1])
+                    if operator == '+':
+                        result = operand1 + operand2
+                    elif operator == '-':
+                        result = operand1 - operand2
+                    elif operator == '*':
+                        result = operand1 * operand2
+                    elif operator == '/':
+                        result = operand1 / operand2 if operand2 != 0 else "Cannot divide by zero."
+                    elif operator == '%':
+                        result = operand1 % operand2
+                    print(f"The result of {calculation} is {result}")
+                    break
+        else:
+            print("Invalid, try again")
+    except Exception as e:
+        print(f"Error: {e}. Please enter a valid calculation.")
+#pretty sure that i did this right too, i ran the code and it seems to work
 
         
